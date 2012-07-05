f3df9538cc78405b4deeb2d92e7297d9fc9166a0
yuanjin <wangjl0625@gmail.com>
2012-07-05 15:08:54 +0800

    修改

--- app.js
+++ app.js
@@ -30,9 +30,7 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
-app.get('/abcde', function(req,res){
-    res.end('sadfasdfasdfsad')
-});
+
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
